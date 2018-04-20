0daaabf01ee3c7589432615520ba469acd781785
Red <alexis.aformac@gmail.com>
2018-04-20 09:18:31 +0200

    recup

--- salle_un.php
+++ salle_un.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
+
 <html>
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-       <link rel="stylesheet" type="text/css" href="css/salle_un.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="css/salle_un.css" media="all" />
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <title>Salle 1</title>
     </head>
+
     <body>
-     <div id="tel">
-         <img src="img/objets/telephone-ascensseur.png" alt="">
+
+     <div class="img-fluid" id="image" >
+        <img src="img/ascenceur.png" alt="">
+        <img id="hache" src="img/objets/hache_.png" alt="">
+        <img id="tel" src="img/objets/telephone-ascensseur.png" alt="">
+        <img id="hp" src="img/objets/haut_parleur.png" alt="">
      </div>
 
-     <div id="hp">
-         <img src="img/objets/haut_parleur.png" alt="">
-     </div>
-
-     <div id="hache">
-         <img src="img/objets/hache_.png" alt="">
-     </div>
     </body>
+
 </html>
